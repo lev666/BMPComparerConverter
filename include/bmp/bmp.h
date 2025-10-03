91ef60246da06c142aad1faeb25760040c993ef4
lev666 <opo.rshr3@gmail.com>
2025-10-03 22:37:29 +0500

    refactor: fixed compliance with the Google Coding Style standard

---
+++ include/bmp/bmp.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#pragma pack(push, 1) // отключение оптимизации выравнивания типов
+
+#include <stdint.h>
+
+typedef struct {
+    uint16_t Signature;
+    uint32_t FileSize;
+    uint32_t Reserved;
+    uint32_t DataOffset;
+} BMPHeader;
+
+typedef struct {
+    uint32_t Size;
+    int32_t Width;
+    int32_t Height;
+    uint16_t Planes;
+    uint16_t BitCount;
+    uint32_t Compression;
+    uint32_t ImageSize;
+    int32_t XpixelsPerM;
+    int32_t YpixelsPerM;
+    uint32_t ColorsUsed;
+    uint32_t ColorsImportant;
+} BMPInfoHeader;
+
+typedef struct {
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+    uint8_t reserved;
+} BMPColor;
+
+typedef struct {
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+} CurrBMPPixel; // для 24 bit
+
+typedef struct {
+    BMPHeader header;
+    BMPInfoHeader infoHeader;
+    BMPColor* colorTable; // для 8 bit
+    uint8_t* data; // для 8/24 bit
+} BMPImage;
+
+#pragma pack(pop); // возврат настроек 
+
