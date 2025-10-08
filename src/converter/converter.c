6a4e4bd1a0c43f8a7f20ad71d02b0a92c091b041
lev666 <opo.rshr3@gmail.com>
2025-10-08 21:06:51 +0500

    fix: fixed incorrect command processing and minor fix

--- src/converter/converter.c
+++ src/converter/converter.c
@@ -6,21 +6,14 @@
 int invert_bmp(BMPImage* image);
 
 int main(int argc, char** argv) {
-    if (argc != 4) {
+    if (argc != 3) {
         fprintf(stderr, "Error: Not enough arguments.\n");
-        return 1;
-    }
-
-    char* command = argv[1];
-
-    if (strcmp(command, "converter") != 0) {
-        fprintf(stderr, "Error: Incorrect utility argument.\n");
         return 1;
     }
 
     BMPImage image;
 
-    if (read_bmp(argv[2], &image) != 0) {
+    if (read_bmp(argv[1], &image) != 0) {
         fprintf(stderr, "Error: Unable to read file.\n");
         free_bmp(&image);
         return 1;
@@ -32,7 +25,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    if (write_bmp(argv[3], &image) != 0 ) {
+    if (write_bmp(argv[2], &image) != 0 ) {
         fprintf(stderr, "Error: Failed to write new data.\n");
         free_bmp(&image);
         return 1;
@@ -40,12 +33,12 @@ int main(int argc, char** argv) {
 }
 
 int invert_bmp(BMPImage* image) {
-    int8_t rev = 0xFF; // 11111111
+    int8_t rev = -1; // 11111111
     if (image -> infoHeader.bitCount == 8) {
         for (size_t i = 0; i < 256; i++) {
-            image -> colorTable[i].blue = (image -> colorTable[i].blue) ^ rev;
-            image -> colorTable[i].green = (image -> colorTable[i].green) ^ rev;
-            image -> colorTable[i].red = (image -> colorTable[i].red) ^ rev;
+            image -> colorTable[i].blue ^= rev;
+            image -> colorTable[i].green ^= rev;
+            image -> colorTable[i].red ^= rev;
         }
     } else if (image -> infoHeader.bitCount == 24) {
         int width = image -> infoHeader.width;
@@ -59,9 +52,9 @@ int invert_bmp(BMPImage* image) {
         for (size_t y = 0; y < height; y++) {
             CurrBMPPixel* row = (CurrBMPPixel*)(data_ptr + y * stride);
             for (size_t x = 0; x < width; x++) {
-                row[x].blue = row[x].blue ^ rev;
-                row[x].green = row[x].green ^ rev;
-                row[x].red = row[x].red ^ rev;
+                row[x].blue ^= rev;
+                row[x].green ^= rev;
+                row[x].red ^= rev;
             }
         }
     } else {
