0eeeb97ff1f7f2a3d497bd92f1ba838acba47712
lev666 <opo.rshr3@gmail.com>
2025-10-09 22:18:10 +0500

    style: correcting the style code according to the LLVM format

--- include/bmp/bmp.h
+++ include/bmp/bmp.h
@@ -5,50 +5,50 @@
 #include <stdint.h>
 
 typedef struct {
-    uint16_t signature;
-    uint32_t fileSize;
-    uint32_t reserved;
-    uint32_t dataOffset;
+  uint16_t signature;
+  uint32_t fileSize;
+  uint32_t reserved;
+  uint32_t dataOffset;
 } BMPHeader;
 
 typedef struct {
-    uint32_t size;
-    int32_t width;
-    int32_t height;
-    uint16_t planes;
-    uint16_t bitCount;
-    uint32_t compression;
-    uint32_t imageSize;
-    int32_t xPixelsPerM;
-    int32_t yPixelsPerM;
-    uint32_t colorsUsed;
-    uint32_t colorsImportant;
+  uint32_t size;
+  int32_t width;
+  int32_t height;
+  uint16_t planes;
+  uint16_t bitCount;
+  uint32_t compression;
+  uint32_t imageSize;
+  int32_t xPixelsPerM;
+  int32_t yPixelsPerM;
+  uint32_t colorsUsed;
+  uint32_t colorsImportant;
 } BMPInfoHeader;
 
 typedef struct {
-    uint8_t blue;
-    uint8_t green;
-    uint8_t red;
-    uint8_t reserved;
+  uint8_t blue;
+  uint8_t green;
+  uint8_t red;
+  uint8_t reserved;
 } BMPColor;
 
 typedef struct {
-    uint8_t blue;
-    uint8_t green;
-    uint8_t red;
+  uint8_t blue;
+  uint8_t green;
+  uint8_t red;
 } CurrBMPPixel; // для 24 bit
 
 typedef struct {
-    BMPHeader header;
-    BMPInfoHeader infoHeader;
-    BMPColor* colorTable; // для 8 bit
-    uint8_t* data; // для 8/24 bit запись пикселей
+  BMPHeader header;
+  BMPInfoHeader infoHeader;
+  BMPColor *colorTable; // для 8 bit
+  uint8_t *data;        // для 8/24 bit запись пикселей
 } BMPImage;
 
-#pragma pack(pop) // возврат настроек 
+#pragma pack(pop) // возврат настроек
 
-int read_bmp(const char* filename, BMPImage* image);
+int read_bmp(const char *filename, BMPImage *image);
 
-int write_bmp(const char* filename, const BMPImage* image);
+int write_bmp(const char *filename, const BMPImage *image);
 
-void free_bmp(BMPImage* image);
+void free_bmp(BMPImage *image);
--- src/converter/converter.c
+++ src/converter/converter.c
@@ -1,69 +1,69 @@
 #include "bmp.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-int invert_bmp(BMPImage* image);
+int invert_bmp(BMPImage *image);
 
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Error: Not enough arguments.\n");
-        return 1;
-    }
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fprintf(stderr, "Error: Not enough arguments.\n");
+    return 1;
+  }
 
-    BMPImage image = {0};
+  BMPImage image = {0};
 
-    if (read_bmp(argv[1], &image) != 0) {
-        fprintf(stderr, "Error: Unable to read file.\n");
-        free_bmp(&image);
-        return 1;
-    }
-
-    if (invert_bmp(&image) != 0) {
-        fprintf(stderr, "Error: Failed to invert the image.\n");
-        free_bmp(&image);
-        return 1;
-    }
-
-    if (write_bmp(argv[2], &image) != 0 ) {
-        fprintf(stderr, "Error: Failed to write new data.\n");
-        free_bmp(&image);
-        return 1;
-    }
-
+  if (read_bmp(argv[1], &image) != 0) {
+    fprintf(stderr, "Error: Unable to read file.\n");
     free_bmp(&image);
-    return 0;
+    return 1;
+  }
+
+  if (invert_bmp(&image) != 0) {
+    fprintf(stderr, "Error: Failed to invert the image.\n");
+    free_bmp(&image);
+    return 1;
+  }
+
+  if (write_bmp(argv[2], &image) != 0) {
+    fprintf(stderr, "Error: Failed to write new data.\n");
+    free_bmp(&image);
+    return 1;
+  }
+
+  free_bmp(&image);
+  return 0;
 }
 
-int invert_bmp(BMPImage* image) {
-    int8_t rev = -1; // 11111111
-    if (image -> infoHeader.bitCount == 8) {
-        for (size_t i = 0; i < 256; i++) {
-            image -> colorTable[i].blue ^= rev;
-            image -> colorTable[i].green ^= rev;
-            image -> colorTable[i].red ^= rev;
-        }
-    } else if (image -> infoHeader.bitCount == 24) {
-        unsigned int width = image -> infoHeader.width;
-        unsigned int height = abs(image -> infoHeader.height);
-
-        int row_size = width * 3;
-        int padding = (4 - (row_size % 4)) % 4;
-        int stride = row_size + padding; // выравнивание
-
-        uint8_t* data_ptr = image->data;
-        for (size_t y = 0; y < height; y++) {
-            CurrBMPPixel* row = (CurrBMPPixel*)(data_ptr + y * stride);
-            for (size_t x = 0; x < width; x++) {
-                row[x].blue ^= rev;
-                row[x].green ^= rev;
-                row[x].red ^= rev;
-            }
-        }
-    } else {
-        fprintf(stderr, "Error: Unsupported BMP bit depth format.\n");
-        return 1;
+int invert_bmp(BMPImage *image) {
+  int8_t rev = -1; // 11111111
+  if (image->infoHeader.bitCount == 8) {
+    for (size_t i = 0; i < 256; i++) {
+      image->colorTable[i].blue ^= rev;
+      image->colorTable[i].green ^= rev;
+      image->colorTable[i].red ^= rev;
     }
+  } else if (image->infoHeader.bitCount == 24) {
+    unsigned int width = image->infoHeader.width;
+    unsigned int height = abs(image->infoHeader.height);
 
-    return 0;
+    int row_size = width * 3;
+    int padding = (4 - (row_size % 4)) % 4;
+    int stride = row_size + padding; // выравнивание
+
+    uint8_t *data_ptr = image->data;
+    for (size_t y = 0; y < height; y++) {
+      CurrBMPPixel *row = (CurrBMPPixel *)(data_ptr + y * stride);
+      for (size_t x = 0; x < width; x++) {
+        row[x].blue ^= rev;
+        row[x].green ^= rev;
+        row[x].red ^= rev;
+      }
+    }
+  } else {
+    fprintf(stderr, "Error: Unsupported BMP bit depth format.\n");
+    return 1;
+  }
+
+  return 0;
 }
