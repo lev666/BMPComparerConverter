e6017e55219328c949a20f23c786c3e04a13ff74
lev666 <opo.rshr3@gmail.com>
2025-10-12 21:40:42 +0500

    build: initial testing support during project assembly

---
+++ scripts/tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+BASEDIR=$(realpath ""$(dirname "$0")"")
+ROOTDIR="$BASEDIR/.."
+BINDIR=$(realpath "$ROOTDIR/build/src")
+TESTDIR=$(realpath "$ROOTDIR/tests")
+
+#echo "Test comparer"
+#python3 "${TESTDIR}/comparer/comparer_checker.py" "${BINDIR}/comparer" "${TESTDIR}/comparer/test_data"
+
+echo "Test converter"
+python3 "${TESTDIR}/converter/converter_checker.py" "${BINDIR}/converter" "${TESTDIR}/converter/test_data" "${BINDIR}/comparer"
